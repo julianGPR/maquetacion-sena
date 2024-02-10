504685ab0f3a9f7aefc333cad57085d471b3717d
nicolasegpla <nicolasedixongarciapineros@gmail.com>
2024-02-09 20:44:29 -0500

    creacion de componente tablaOperarios

---
+++ tablaOperarios/index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operarios</title>
+    <link rel="stylesheet" href="./styles.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <section class="layout">
+        <nav class="nav--admin">
+            <div class="nav--admin-h2">
+                <h2>Industrias Gafra</h2>
+            </div>
+            <div class="nav--admin">
+                <button class="nav--admin__button">Cerras sesion</button>
+                <div class="nav--admin__div">
+                    <img class="nav--admin__div__img" src="../assets/notificacion.png" alt="">
+                    <p class="nav--admin__div__p--notificacion">4</p>
+                </div>
+            </div>
+        </nav>
+        <article class="article--admin">
+            <div class="article--admin__div">
+                <div class="article--admin__div--img__c--img"><img class="article--admin__div--img__c--img__img" src="../assets/Login.png" alt=""></div>
+                <div class="article--admin__div--info">
+                    <p class="article--admin__div--info__usuario">Julian Garcia</p>
+                    <p class="article--admin__div--info__id">C.C:1109096997</p>
+                    <p class="article--admin__div--info__correo">julian@gafra.com</p>
+                </div>
+            </div>
+            <div class="article--admin__div--h2">
+                <h2 class="article--admin__div--h2__h2">Operarios</h2>
+                <input class="login__form__button" type="text" placeholder="Buscar">
+            </div>
+        </article>
+
+        
+        <div class="contenedor--tabla">
+            <button class="contenedor--tabla__button--crear">Crear nuevo operario</button>
+            <table class="tabla">
+                <tr class="tabla__tr">
+                    <td class="tabla__td--check"><input type="checkbox"></td>
+                    <td class="tabla__td">Identificacion</td>
+                    <td class="tabla__td">Nombre</td>
+                    <td class="tabla__td">Correo</td>
+                    <td class="tabla__td">Tipo de Operario</td>
+                    <td class="tabla__td">Usuario</td>
+                    <td class="tabla__td">Clave</td>
+                </tr>
+                <tr class="tabla__tr">
+                    <td class="tabla__td--check"><input type="checkbox"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                </tr>
+                <tr class="tabla__tr">
+                    <td class="tabla__td--check"><input type="checkbox"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                </tr>
+                <tr class="tabla__tr">
+                    <td class="tabla__td--check"><input type="checkbox"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                </tr>
+                <tr class="tabla__tr">
+                    <td class="tabla__td--check"><input type="checkbox"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                </tr>
+                <tr class="tabla__tr">
+                    <td class="tabla__td--check"><input type="checkbox"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                </tr>
+                <tr class="tabla__tr">
+                    <td class="tabla__td--check"><input type="checkbox"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                </tr>
+                <tr class="tabla__tr">
+                    <td class="tabla__td--check"><input type="checkbox"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                </tr>
+                <tr class="tabla__tr">
+                    <td class="tabla__td--check"><input type="checkbox"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                    <td class="tabla__td"></td>
+                </tr>
+                
+                
+    
+            </table>
+            <div class="contenedor--tabla__botones">
+                <button class="contenedor--tabla__button--crear">Bloquear</button>
+                <button class="contenedor--tabla__button--crear">Editar</button>
+                <button class="contenedor--tabla__button--crear">Borrar</button>
+
+            </div>
+
+        </div>
+
+        
+        
+
+        <div class="button--back__div">
+            <div class="button--back">
+                <img class="button--back__img" src="../assets/back.png" alt="">
+            </div>
+            <div class="button--back">
+                <p class="button--back__p">Inicio</p>
+            </div>
+
+        </div>
+        
+        <p class="layout__p--reservados">2024®️ Gafra todos los derechos reservados</p>
+    </section>
+    
+</body>
+</html>
